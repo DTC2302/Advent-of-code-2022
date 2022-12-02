c76f9c9ac5443373bd68486b4f6374c267095e63
DTC2302 <darkdragonfire23@gmail.com>
2022-12-02 00:36:23 -0600

    day2

---
+++ Rock Paper Scisors(part 2).py
@@ -0,0 +1,32 @@
+games = []
+while 1:
+    try:
+        games.append(input().split())
+    except EOFError:
+        break
+score = 0
+for i in games:
+    if i[1] == 'X':
+        if i[0] == 'A':
+            score+=3
+        elif i[0] == 'B':
+            score+=1
+        else:
+            score+=2
+    elif i[1] == 'Y':
+        score+=3
+        if i[0] == 'A':
+            score+=1
+        elif i[0] == 'B':
+            score+=2
+        else:
+            score+=3
+    elif i[1] == 'Z':
+        score+=6
+        if i[0] == 'A':
+            score+=2
+        elif i[0] == 'B':
+            score+=3
+        else:
+            score+=1
+print(score)
