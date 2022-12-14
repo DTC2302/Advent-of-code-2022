37ab3ecd85655d2ad219c7514ae8dd3196971bef
DTC2302 <darkdragonfire23@gmail.com>
2022-12-14 01:51:42 -0600

    day 13

--- day13/Distress Signal(part 2).py
+++ day13/Distress Signal(part 2).py
@@ -19,7 +19,19 @@ def ordered(left, right):
         return True
     elif len(right)<len(left):
         return False
-                    
+
+def advancedSort(x):
+    temp = [x.pop(0)]
+    for i in x:
+        for j in temp:
+            if ordered(i, j):
+                temp.insert(temp.index(j), i)
+                break
+        else:
+            temp.append(i)
+    return temp
+
+
 with open(r'C:\Users\darkd\OneDrive\Documents\Code\GitHub\input.txt', 'r') as f:
     lines = f.readlines()
 for i in lines:
@@ -31,3 +43,5 @@ allPackets = [[[2]], [[6]]]
 for i in range(0,len(lines), 3):
     allPackets.append(lines[i])
     allPackets.append(lines[i+1])
+allPackets = advancedSort(allPackets)
+print((allPackets.index([[6]])+1)*(allPackets.index([[2]])+1))
